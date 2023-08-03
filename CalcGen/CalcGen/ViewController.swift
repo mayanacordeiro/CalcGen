f9bd52806bc467a46f96407ba4a5c7334881427e
Mayana Cordeiro <mayanacoramos@gmail.com>
2023-08-03 20:02:02 -0300

    feat(feature/calculate-code-2: created calculateFG func

--- CalcGen/CalcGen/ViewController.swift
+++ CalcGen/CalcGen/ViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var labelTwo: UILabel!
     @IBOutlet weak var textFieldOne: UITextField!
     @IBOutlet weak var textFieldTwo: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    @IBOutlet weak var sgementedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        segmentedControlLabel()
     }
     
     
@@ -26,23 +26,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateButton(_ sender: UIButton) {
+        calculateFG()
     }
     
     @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        segmentedControlLabel()
     }
-//
-//    func segmentedControl() {
-//        if segmentedControl.selectedSegmentIndex == 0 {
-//            labelOne.text = "Número de homozigotos"
-//            labelTwo.text = "Número de heterozigotos"
-//        } else {
-//            labelOne.text = "Total de indivíduos com genótipo"
-//            labelTwo.text = "Total de indivíduos da população"
-//        }
-//
-//    }
+    
+
+    func segmentedControlLabel() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            labelOne.text = "Número de homozigotos"
+            labelTwo.text = "Número de heterozigotos"
+        } else {
+            labelOne.text = "Total de indivíduos com genótipo"
+            labelTwo.text = "Total de indivíduos da população"
+        }
+
+    }
     
     
+    func calculateFG() -> Float {
+        let inputTextFieldOne = textFieldOne.text!
+        let inputOneFloat = Float(inputTextFieldOne)!
+        
+        let inputTextFieldTwo = textFieldTwo.text!
+        let inputTwoFloat = Float(inputTextFieldTwo)!
+        
+        let FG = inputOneFloat / inputTwoFloat
+//        let FGRounded = round(FG * 100) / 100
+        
+        print(FG)
+        
+        return FG
+    }
     
     
 }
