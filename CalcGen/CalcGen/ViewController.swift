5b884c6d096e4b2098061ca0cd50f8677cdacf3d
Mayana Cordeiro <mayanacoramos@gmail.com>
2023-08-29 14:45:54 -0300

    adds UIAlert e UITextFieldDelegate

--- CalcGen/CalcGen/ViewController.swift
+++ CalcGen/CalcGen/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textFieldOne: UITextField!
     @IBOutlet weak var textFieldTwo: UITextField!
-    
     @IBOutlet weak var textFieldThree: UITextField!
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -31,11 +30,22 @@ class ViewController: UIViewController {
         segmentedControlLabel()
         setupConstraints()
         
-                
+        textFieldOne.delegate = self
+        textFieldTwo.delegate = self
+        textFieldThree.delegate = self
+        
+        calculateButton.isEnabled = false
+        calculateButton.setTitleColor(.white.withAlphaComponent(0.95), for: .disabled)
+        calculateButton.setTitleColor(.white, for: .normal)
     }
     
     
     @IBAction func infoButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Atenção", message: "Os resultados da calculadora serão arredondados para até 5 casas decimais quando necessário. ", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
+        
+        self.present(alert,animated: true)
     }
     
     @IBAction func calculateButton(_ sender: UIButton) {
@@ -64,6 +74,10 @@ class ViewController: UIViewController {
         labelResult.isHidden = true
         calculateButton.isHidden = false
         
+        self.textFieldOne.text = nil
+        self.textFieldTwo.text = nil
+        self.textFieldThree.text = nil
+        
         
     }
     
@@ -86,23 +100,7 @@ class ViewController: UIViewController {
 
     }
     
-    
-    func calculateFG() -> Float {
-        let inputTextFieldOne = textFieldOne.text!
-        let inputOneFloat = Float(inputTextFieldOne)!
-        
-        let inputTextFieldTwo = textFieldTwo.text!
-        let inputTwoFloat = Float(inputTextFieldTwo)!
-        
-        let FG = inputOneFloat / inputTwoFloat
-//        let FGRounded = round(FG * 100) / 100
-        
-        print(FG)
-        
-        return FG
-    }
-    
-    func calculateFA() -> Float {
+    func calculateFA() -> String {
         let inputTextFieldOne = textFieldOne.text!
         let inputOneFloat = Float(inputTextFieldOne)!
         
@@ -113,17 +111,34 @@ class ViewController: UIViewController {
         let inputThreeFloat = Float(inputTextFieldThree)!
         
         let FA = ((inputOneFloat * 2) + inputTwoFloat) / (inputThreeFloat * 2)
+        let FAString = String(format: "%.5f", FA)
+        //print(FA)
         
-        print(FA)
-        
-        return FA
+        return FAString
         
     }
     
-    func showResult(de valor: Float) {
+    
+    func calculateFG() -> String {
+        let inputTextFieldOne = textFieldOne.text!
+        let inputOneFloat = Float(inputTextFieldOne)!
+        
+        let inputTextFieldTwo = textFieldTwo.text!
+        let inputTwoFloat = Float(inputTextFieldTwo)!
+        
+        let FG = inputOneFloat / inputTwoFloat
+//        let FGRounded = round(FG * 100) / 100.00000
+        let FGString = String(format: "%.5f", FG)
+        //print(FG)
+        
+        return FGString
+    }
+    
+    
+    func showResult(de valor: String) {
         //exibe resultado na label
         labelResult.isHidden = false
-        labelResult.text = "O valor da FG é \(valor)"
+        labelResult.text = "Resultado: \(valor)"
         
         calculateButton.isHidden = false
         clearButton.isHidden = false
@@ -143,25 +158,64 @@ class ViewController: UIViewController {
             NSLayoutConstraint.activate([calculateButton.topAnchor.constraint(equalTo: textFieldThree.bottomAnchor, constant: 32)
                                          ])
 
-        } else {
-            labelResult.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([labelResult.topAnchor.constraint(equalTo: textFieldTwo.bottomAnchor, constant: 48)
-                                        ])
-
-            clearButton.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([clearButton.topAnchor.constraint(equalTo: labelResult.bottomAnchor, constant: 24)
-                                        ])
-        
-            calculateButton.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([calculateButton.topAnchor.constraint(equalTo: textFieldTwo.bottomAnchor, constant: 32)
-                                        ])
-            
         }
+//        else {
+//            labelResult.translatesAutoresizingMaskIntoConstraints = false
+//            NSLayoutConstraint.activate([labelResult.topAnchor.constraint(equalTo: textFieldTwo.bottomAnchor, constant: 48)
+//                                        ])
+//
+//            clearButton.translatesAutoresizingMaskIntoConstraints = false
+//            NSLayoutConstraint.activate([clearButton.topAnchor.constraint(equalTo: labelResult.bottomAnchor, constant: 24)
+//                                        ])
+//        
+//            calculateButton.translatesAutoresizingMaskIntoConstraints = false
+//            NSLayoutConstraint.activate([calculateButton.topAnchor.constraint(equalTo: textFieldTwo.bottomAnchor, constant: 32)
+//                                        ])
+//            
+//        }
 
+    }
+    
+    func validateTextFields() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            if textFieldOne.text != "" && textFieldTwo.text != "" && textFieldThree.text != "" {
+                calculateButton.isEnabled = true
+            } else {
+                calculateButton.isEnabled = false
+            }
+        } else {
+            if textFieldOne.text != "" && textFieldTwo.text != "" {
+                calculateButton.isEnabled = true
+                
+            } else {
+                calculateButton.isEnabled = false
+            }
+        }
     }
     
     
     
     
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 0
+        validateTextFields()
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+   
 }
 
